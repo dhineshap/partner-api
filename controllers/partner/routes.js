80f033b0e2a66acc049fbee35ed235052e172203
dhineshap <apdhinesh322@gmail.com>
2026-03-13 10:20:41 +0530

    partner changes

--- controllers/partner/routes.js
+++ controllers/partner/routes.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const { getSwaggerDocs } = require('./docs')
+const { getSwaggerDocs, swaggerSpec, swaggerUi } = require('./docs')
 
-router.get('/shipment/docs.json', getSwaggerDocs)
-
+router.get(
+    '/shipment/docs',
+    swaggerUi.setup(swaggerSpec, {
+        explorer: true,
+        customCss: '.swagger-ui .topbar { display: none }',
+        customSiteTitle: "Partner API Documentation",
+        swaggerOptions: {
+            url: '/partner/shipment/docs.json',
+            persistAuthorization: true,
+            displayRequestDuration: true,
+            filter: true,
+            deepLinking: true
+        }
+    })
+);
 module.exports = router;
--- index.js
+++ index.js
@@ -8,25 +8,7 @@ const { swaggerUi, swaggerSpec } = require('./controllers/partner/docs');
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/partner/shipment/docs',
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerSpec, {
-        explorer: true,
-        customCss: '.swagger-ui .topbar { display: none }',
-        customSiteTitle: "Partner API Documentation",
-        swaggerOptions: {
-            url: '/partner/shipment/docs.json',
-            persistAuthorization: true,
-            displayRequestDuration: true,
-            filter: true,
-            deepLinking: true
-        }
-    })
-);
-
-app.use('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the Partner API!' });
-})
+app.use('/partner/shipment/docs', swaggerUi.serve);
 
 app.use('/partner', require('./controllers/partner/routes'))
 
